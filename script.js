d775cec7d9c3e960c1187be7fd5e1cf384cbda2e
Luis Henrique Gomes Zortea <102925017+LuisHZortea@users.noreply.github.com>
2022-06-07 14:16:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function registro(){
 function login(){
   location.replace("https://luishzortea.github.io/foreignhtml/login.html")
 }
+
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
