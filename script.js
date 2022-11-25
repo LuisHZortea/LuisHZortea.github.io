993e7d78e1bde9d58c1675ab6c52225cbd79429b
Luis Henrique Gomes Zortea <102925017+LuisHZortea@users.noreply.github.com>
2022-11-25 15:52:18 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function registro(){
-  location.replace("https://luishzortea.github.io/foreignhtml/registro.html")
-}
-
-function login(){
-  location.replace("https://luishzortea.github.io/foreignhtml/login.html")
-}
